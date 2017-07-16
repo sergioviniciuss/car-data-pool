ee08685ed4f3d773af3d456f2ace2ae2528ce087
sergioviniciuss <sergioviniciuss@gmail.com>
2017-07-15 23:20:05 -0300

    Adding dynamic filter based on mocked api data

---
+++ src/api/mockCarsApi.js
@@ -0,0 +1,61 @@
+const synonyms = [
+	{
+		volkswagen: ["vw", "wagen"]
+	},
+	{
+		audi: ["power","A1","A3"]
+	}
+];
+
+const cars = [
+	{
+		make: "Volkswagen",
+		model: "Fusca",
+		year: "1991"
+	},
+	{
+		make: "Volkswagen",
+		model: "New Beatle",
+		year: "2002"
+	},
+	{
+		make: "Volkswagen",
+		model: "Voyage",
+		year: "1995"
+	},
+	{
+		make: "Audi",
+		model: "A3",
+		year: "2000"
+	},
+	{
+		make: "Audi",
+		model: "A4",
+		year: "2004"
+	},
+	{
+		make: "Audi",
+		model: "TT",
+		year: "2014"
+	},
+	{
+		make: "Mercedes",
+		model: "SLK",
+		year: "2001"
+	},
+	{
+		make: "Mercedes",
+		model: "CLA",
+		year: "2002"
+	}
+];
+
+export default class CarsApi{
+	static getAllCars() {
+		return cars;
+	}
+	static getSynonyms() {
+		return synonyms;
+	}
+}
+
--- src/components/CarFilter.js
+++ src/components/CarFilter.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+import mockCarsApi from "../api/mockCarsApi";
 
-class CarSearch extends Component {
+class CarFilter extends Component {
+	constructor(props){
+		super(props);
+	
+		this.state = {
+			 initialItems : this.getCars(),
+			 listedItems: []
+		};
+
+		this.getCars = this.getCars.bind(this);
+		this.dynamicFilter = this.dynamicFilter.bind(this);
+	}
+
+	componentWillMount() {
+		this.setState({
+			listedItems: this.state.initialItems
+		});
+	}
+	getCars() {
+		let cars = mockCarsApi.getAllCars();
+		return cars;
+	}
+
+	dynamicFilter() {
+		console.log("car filter...");
+		let searchedWord = document.querySelector('#search').value;
+
+		let filteredRows = this.state.initialItems;
+
+		filteredRows = filteredRows.filter((car)=>{
+			return car.make.toLowerCase().search(searchedWord.toLowerCase()) !== -1;
+		});
+		this.setState({
+			listedItems: filteredRows
+		})
+	}
+
+
 	render() {
 		return(
 			<div>
-				<table>
+				<div className="">
+					<input id="search" type="search" placeholder="type to search" onChange={this.dynamicFilter}/>
+				</div>
+				<table id="cars-list">
 					<thead>
 						<tr>
 							<th>Make</th>
@@ -12,47 +53,16 @@ class CarSearch extends Component {
 							<th>Year</th>
 						</tr>
 					</thead>
-					<tbody className="teste">
-						<tr>
-							<td>Volkswagen</td>
-							<td>Fusca</td>
-							<td>1991</td>
-						</tr>
-						<tr>
-							<td>Volkswagen</td>
-							<td>New Beatle</td>
-							<td>2002</td>
-						</tr>
-						<tr>
-							<td>Volkswagen</td>
-							<td>Voyage</td>
-							<td>1995</td>
-						</tr>
-						<tr>
-							<td>Audi</td>
-							<td>A3</td>
-							<td>2000</td>
-						</tr>
-						<tr>
-							<td>Audi</td>
-							<td>A4</td>
-							<td>2004</td>
-						</tr>
-						<tr>
-							<td>Audi</td>
-							<td>TT</td>
-							<td>2014</td>
-						</tr>
-						<tr>
-							<td>Mercedes</td>
-							<td>SLK</td>
-							<td>2001</td>
-						</tr>
-						<tr>
-							<td>Mercedes</td>
-							<td>CLA</td>
-							<td>2002</td>
-						</tr>
+					<tbody className="">
+						{ 
+							this.state.listedItems.map((car, index) =>
+								<tr key={index.toString()}>
+									<td>{car.make}</td>
+									<td>{car.model}</td>
+									<td>{car.year}</td>
+								</tr>
+							)
+						}
 					</tbody>
 				</table>
 			</div>
@@ -60,4 +70,4 @@ class CarSearch extends Component {
 	}
 }
 
-export default CarSearch;
+export default CarFilter;
